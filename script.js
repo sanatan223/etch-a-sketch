3aad86a62b881d22751c5af645d545b18f74d790
sanatan223 <sanatan_dev@outlook.com>
2024-07-24 16:23:41 +0530

    made the function of the pen

--- script.js
+++ script.js
@@ -2,19 +2,35 @@
 
 const htmlPlayground = document.querySelector(".js-playground")
 
-function generateDivs(){
+function generatePixels(){
+
     for (let i = 0; i < 16; i++){
         htmlPlayground.innerHTML += `<div class="js-line-of-pixel pixelLine"></div>`
     }
-    console.log('lines created')
+
     const htmlPixelLines = document.querySelectorAll(".js-line-of-pixel")
+
     htmlPixelLines.forEach((eachLine) => {
+
         for (let i = 0; i <16; i++){
-            eachLine.innerHTML += `<div class="pixel"></div>`
+            eachLine.innerHTML += `<div class="js-pixels pixel"></div>`
         }
+
     })
 }
 
+function pen(){
 
+    const htmlPixels = document.querySelectorAll(".js-pixels")
 
-generateDivs();
+    htmlPixels.forEach((eachPixel) => {
+
+        eachPixel.addEventListener('mousemove',() => {
+            eachPixel.classList.add('active-pixel')
+        })
+
+    })
+}
+
+generatePixels();
+pen();
